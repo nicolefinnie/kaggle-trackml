2711b2387ba8f5fd36f682e70c5759602f2bd0ed
Liam Finnie <lfinnie@de.ibm.com>
2018-05-28 22:36:06 +0200

    Add support to partition test predictions.

--- src/unrolldbscan_Luis.py
+++ src/unrolldbscan_Luis.py
@@ -1,54 +1,54 @@
-import numpy as np
+# import numpy as np
 
-x = np.array([(62,397,103),(82,347,107),(93,288,120),
-     (94,266,128),(65,163,169),(12,102,198),
-     (48,138,180),(77,187,157),(85,209,149),(89,316,112)])
+# x = np.array([(62,397,103),(82,347,107),(93,288,120),
+#      (94,266,128),(65,163,169),(12,102,198),
+#      (48,138,180),(77,187,157),(85,209,149),(89,316,112)])
 
-xm = np.mean(x)
+# xm = np.mean(x)
 
-X = (x-xm).T
-
-
-v, s, t = np.linalg.svd(X,full_matrices=True)
+# X = (x-xm).T
 
 
-sigma1 = s[0]
-sigma2 = s[1]
-sigma3 = s[2]
-v1 = t[0]
-v2 = t[1]
-v3 = t[2]
+# v, s, t = np.linalg.svd(X,full_matrices=True)
 
-Z = np.zeros((x.shape[0],10), np.float32)
-Z[:,0] = x[:,0]**2
-Z[:,1] = 2*x[:,0]*x[:,1]
-Z[:,2] = 2*x[:,0]*x[:,2]
-Z[:,3] = 2*x[:,0]
-Z[:,4] = x[:,1]**2
-Z[:,5] = 2*x[:,1]*x[:,2]
-Z[:,6] = 2*x[:,1]
-Z[:,7] = x[:,2]**2
-Z[:,8] = 2*x[:,2]
-Z[:,9] = 1
 
-v, s, t = np.linalg.svd(Z,full_matrices=True)
-smallest_value = np.min(np.array(s))
-smallest_index = np.argmin(np.array(s))
-T = np.array(t)
-T = T[smallest_index,:]
-S = np.zeros((4,4),np.float32)
-S[0,0] = T[0]
-S[0,1] = S[1,0] = T[1]
-S[0,2] = S[2,0] = T[2]
-S[0,3] = S[3,0] = T[3]
-S[1,1] = T[4]
-S[1,2] = S[2,1] = T[5]
-S[1,3] = S[3,1] = T[6]
-S[2,2] = T[7]
-S[2,3] = S[3,2] = T[8]
-S[3,3] = T[9]
-norm = np.linalg.norm(np.dot(Z,T), ord=2)**2
-print(norm)
+# sigma1 = s[0]
+# sigma2 = s[1]
+# sigma3 = s[2]
+# v1 = t[0]
+# v2 = t[1]
+# v3 = t[2]
+
+# Z = np.zeros((x.shape[0],10), np.float32)
+# Z[:,0] = x[:,0]**2
+# Z[:,1] = 2*x[:,0]*x[:,1]
+# Z[:,2] = 2*x[:,0]*x[:,2]
+# Z[:,3] = 2*x[:,0]
+# Z[:,4] = x[:,1]**2
+# Z[:,5] = 2*x[:,1]*x[:,2]
+# Z[:,6] = 2*x[:,1]
+# Z[:,7] = x[:,2]**2
+# Z[:,8] = 2*x[:,2]
+# Z[:,9] = 1
+
+# v, s, t = np.linalg.svd(Z,full_matrices=True)
+# smallest_value = np.min(np.array(s))
+# smallest_index = np.argmin(np.array(s))
+# T = np.array(t)
+# T = T[smallest_index,:]
+# S = np.zeros((4,4),np.float32)
+# S[0,0] = T[0]
+# S[0,1] = S[1,0] = T[1]
+# S[0,2] = S[2,0] = T[2]
+# S[0,3] = S[3,0] = T[3]
+# S[1,1] = T[4]
+# S[1,2] = S[2,1] = T[5]
+# S[1,3] = S[3,1] = T[6]
+# S[2,2] = T[7]
+# S[2,3] = S[3,2] = T[8]
+# S[3,3] = T[9]
+# norm = np.linalg.norm(np.dot(Z,T), ord=2)**2
+# print(norm)
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -61,14 +61,12 @@ import multiprocessing as mp
 import threading as thr
 
 
-# Change this according to your directory preferred setting
-path_to_train = "../input/train_1"
 
 
 # This event is in Train_1
-event_prefix = "event000001000"
+##event_prefix = "event000001000"
 
-hits, cells, particles, truth = load_event(os.path.join(path_to_train, event_prefix))
+#hits, cells, particles, truth = load_event(os.path.join(path_to_train, event_prefix))
 
 
 from sklearn.preprocessing import StandardScaler
@@ -76,6 +74,7 @@ import hdbscan
 from scipy import stats
 from tqdm import tqdm
 from sklearn.cluster import DBSCAN
+import argparse
 
 class Clusterer(object):
     def __init__(self,rz_scales=[0.4, 1.6, 0.5]):                        
@@ -84,8 +83,8 @@ class Clusterer(object):
     def _eliminate_outliers(self,labels,M):
         norms=np.zeros((len(labels)),np.float32)
         indices=np.zeros((len(labels)),np.float32)
-        #for i, cluster in tqdm(enumerate(labels),total=len(labels)):
-        for i, cluster in enumerate(labels):
+        #for i, cluster in enumerate(labels):
+        for i, cluster in tqdm(enumerate(labels),total=len(labels)):
             if cluster == 0:
                 continue
             index = np.argwhere(self.clusters==cluster)
@@ -147,8 +146,8 @@ class Clusterer(object):
         dz = 0.000015
         stepdz = 0.000001
         steps = 70
-        #for ii in tqdm(range(steps)):
-        for ii in range(steps):
+        #for ii in range(steps):
+        for ii in tqdm(range(steps)):
             dz = dz + ii*stepdz
             dfh['a1'] = dfh['a0']+dz*dfh['z']*np.sign(dfh['z'].values)
             dfh['x1'] = dfh['a1']/dfh['z1']
@@ -174,8 +173,8 @@ class Clusterer(object):
                 dfh['N1'] = dfh.groupby('s1')['s1'].transform('count')
         dz = 0.000015
         stepdz = -0.000001
-        #for ii in tqdm(range(steps)):
-        for ii in range(steps):
+        #for ii in range(steps):
+        for ii in tqdm(range(steps)):
             dz = dz + ii*stepdz
             dfh['a1'] = dfh['a0']+dz*dfh['z']*np.sign(dfh['z'].values)
             dfh['x1'] = dfh['a1']/dfh['z1']
@@ -224,18 +223,20 @@ def predict_create_submission_in_batch(events_id_list, hits_list, truth_list):
     scores = []
     result_submissions = [None]*num_threads
     result_scores = [None]*num_threads
+    models = [None]*num_threads
+    labels = [None]*num_threads
     print('Using ' + str(num_threads) + ' Threads')
 
-    def parallel_thread(event_id, hits, truth, result_submissions, result_scores, index):
+    def parallel_thread(event_id, hits, truth, result_submissions, result_scores, models, labels, index):
         print('Starting the worker of the event ' + str(event_id) + ' with index ' + str(index))
-        model = Clusterer()
+        models[index] = Clusterer()
         # a list of labels 
-        labels = model.predict(hits)
+        labels[index] = models[index].predict(hits)
         print('Finished the worker of the event ' + str(event_id) + ' with index ' + str(index))
 
         # Prepare submission for an event
         print('Creating submission of the event ' + str(event_id) + ' with index ' + str(index))
-        result_submissions[index] = create_one_event_submission(event_id, hits, labels)
+        result_submissions[index] = create_one_event_submission(event_id, hits, labels[index])
 
         # Score for the event
         if truth is not None:
@@ -247,7 +248,7 @@ def predict_create_submission_in_batch(events_id_list, hits_list, truth_list):
 
     for ii in range(num_threads):
         #event_id, hits, _, _, truth = load_dataset(path_to_train, skip=ii, nevents=1)
-        thread = thr.Thread(target=parallel_thread, args=[events_id_list[ii], hits_list[ii], truth_list[ii], result_submissions, result_scores, ii])
+        thread = thr.Thread(target=parallel_thread, args=[events_id_list[ii], hits_list[ii], truth_list[ii], result_submissions, result_scores, models, labels, ii])
         thread.start()
         threads.append(thread)
 
@@ -260,62 +261,89 @@ def predict_create_submission_in_batch(events_id_list, hits_list, truth_list):
     return submissions, scores
 
 
-event_id_list = []
-hits_list = []
-truth_list = []
+def run_training_in_threads():
+    event_id_list = []
+    hits_list = []
+    truth_list = []
+    # Change this according to your directory preferred setting
+    path_to_train = "../input/train_1"
 
-for event_id, hits, cells, particles, truth in load_dataset(path_to_train, skip=0, nevents=5):
-    event_id_list.append(event_id)
-    hits_list.append(hits)
-    truth_list.append(truth)
+    for event_id, hits, cells, particles, truth in load_dataset(path_to_train, skip=0, nevents=5):
+       event_id_list.append(event_id)
+       hits_list.append(hits)
+       truth_list.append(truth)
 
-submissions, scores = predict_create_submission_in_batch(event_id_list, hits_list, truth_list)
-for i in range(len(submissions)):
-    #print(submissions[i])
-    #print(scores[i])
-    print("Score for event %d: %.8f" % (event_id_list[i], scores[i]))
+    submissions, scores = predict_create_submission_in_batch(event_id_list, hits_list, truth_list)
+    for i in range(len(submissions)):
+       #print(submissions[i])
+       #print(scores[i])
+       print("Score for event %d: %.8f" % (event_id_list[i], scores[i]))
 
+def run_single_threaded_training(skip, nevents):
+    path_to_train = "../input/train_1"
+    dataset_submissions = []
+    dataset_scores = []
 
-# dataset_submissions = []
-# dataset_scores = []
-
-
-# for event_id, hits, cells, particles, truth in load_dataset(path_to_train, skip=0, nevents=5):
-#     # Track pattern recognition
-#     model = Clusterer()
-#     labels = model.predict(hits)
-
-#     # Prepare submission for an event
-#     one_submission = create_one_event_submission(event_id, hits, labels)
-#     dataset_submissions.append(one_submission)
-
-#     # Score for the event
-#     score = score_event(truth, one_submission)
-#     dataset_scores.append(score)
-
-#     print("Score for event %d: %.8f" % (event_id, score))
-
-# print('Mean score: %.8f' % (np.mean(dataset_scores)))
-
-
-path_to_test = "../input/test"
-test_dataset_submissions = []
-
-create_submission = False # True for submission 
-if create_submission:
-    for event_id, hits, cells in load_dataset(path_to_test, parts=['hits', 'cells']):
-
-        # Track pattern recognition 
+    for event_id, hits, cells, particles, truth in load_dataset(path_to_train, skip=skip, nevents=nevents):
+        # Track pattern recognition
         model = Clusterer()
         labels = model.predict(hits)
 
         # Prepare submission for an event
         one_submission = create_one_event_submission(event_id, hits, labels)
-        test_dataset_submissions.append(one_submission)
-        
-        print('Event ID: ', event_id)
+        dataset_submissions.append(one_submission)
 
-    # Create submission file
-    submission = pd.concat(test_dataset_submissions, axis=0)
-    submission.to_csv('submission.csv', index=False)
+        # Score for the event
+        score = score_event(truth, one_submission)
+        dataset_scores.append(score)
+
+        print("Score for event %d: %.8f" % (event_id, score))
+
+    print('Mean score: %.8f' % (np.mean(dataset_scores)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test', nargs=2, type=int)
+    parser.add_argument('--training', nargs=2, type=int)
+    args = parser.parse_args()
+    test_skip = 0
+    test_events = 0
+    training_skip = 0
+    training_events = 0
+
+    if args.test is not None:
+        test_skip = args.test[0]
+        test_events = args.test[1]
+
+    if args.training is not None:
+        training_skip = args.training[0]
+        training_events = args.training[1]
+
+    if not training_events == 0:
+        run_single_threaded_training(training_skip, training_events)
+
+    path_to_test = "../input/test"
+    test_dataset_submissions = []
+
+    #create_submission = True # True for submission 
+    if test_events > 0:
+        print('Predicting test results, skip: %d, events: %d' % (test_skip, test_events))
+        use_header = (test_skip == 0)
+        for event_id, hits, cells in load_dataset(path_to_test, skip=test_skip, nevents=test_events, parts=['hits', 'cells']):
+
+            print('Event ID: ', event_id)
+
+            # Track pattern recognition 
+            model = Clusterer()
+            labels = model.predict(hits)
+
+            # Prepare submission for an event
+            one_submission = create_one_event_submission(event_id, hits, labels)
+            test_dataset_submissions.append(one_submission)
+            
+
+        # Create submission file
+        submission = pd.concat(test_dataset_submissions, axis=0)
+        submission_file = 'submission_' + str(test_skip) + '_' + str(test_events) + '.csv'
+        submission.to_csv(submission_file, index=False, header=use_header)
 
