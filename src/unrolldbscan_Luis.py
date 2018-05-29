f8e09b05d7fc89f8170c01a1c4fc14cdba916967
Nicole Finnie <Nicole.Finnie@de.ibm.com>
2018-05-29 20:49:33 +0200

    step 110, replaced DBSCAN algorithm

--- src/unrolldbscan_Luis.py
+++ src/unrolldbscan_Luis.py
@@ -1,55 +1,3 @@
-# import numpy as np
-
-# x = np.array([(62,397,103),(82,347,107),(93,288,120),
-#      (94,266,128),(65,163,169),(12,102,198),
-#      (48,138,180),(77,187,157),(85,209,149),(89,316,112)])
-
-# xm = np.mean(x)
-
-# X = (x-xm).T
-
-
-# v, s, t = np.linalg.svd(X,full_matrices=True)
-
-
-# sigma1 = s[0]
-# sigma2 = s[1]
-# sigma3 = s[2]
-# v1 = t[0]
-# v2 = t[1]
-# v3 = t[2]
-
-# Z = np.zeros((x.shape[0],10), np.float32)
-# Z[:,0] = x[:,0]**2
-# Z[:,1] = 2*x[:,0]*x[:,1]
-# Z[:,2] = 2*x[:,0]*x[:,2]
-# Z[:,3] = 2*x[:,0]
-# Z[:,4] = x[:,1]**2
-# Z[:,5] = 2*x[:,1]*x[:,2]
-# Z[:,6] = 2*x[:,1]
-# Z[:,7] = x[:,2]**2
-# Z[:,8] = 2*x[:,2]
-# Z[:,9] = 1
-
-# v, s, t = np.linalg.svd(Z,full_matrices=True)
-# smallest_value = np.min(np.array(s))
-# smallest_index = np.argmin(np.array(s))
-# T = np.array(t)
-# T = T[smallest_index,:]
-# S = np.zeros((4,4),np.float32)
-# S[0,0] = T[0]
-# S[0,1] = S[1,0] = T[1]
-# S[0,2] = S[2,0] = T[2]
-# S[0,3] = S[3,0] = T[3]
-# S[1,1] = T[4]
-# S[1,2] = S[2,1] = T[5]
-# S[1,3] = S[3,1] = T[6]
-# S[2,2] = T[7]
-# S[2,3] = S[3,2] = T[8]
-# S[3,3] = T[9]
-# norm = np.linalg.norm(np.dot(Z,T), ord=2)**2
-# print(norm)
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -59,15 +7,6 @@ from trackml.dataset import load_event, load_dataset
 from trackml.score import score_event
 import multiprocessing as mp
 import threading as thr
-
-
-
-
-# This event is in Train_1
-##event_prefix = "event000001000"
-
-#hits, cells, particles, truth = load_event(os.path.join(path_to_train, event_prefix))
-
 
 from sklearn.preprocessing import StandardScaler
 import hdbscan
@@ -83,7 +22,7 @@ class Clusterer(object):
     def _eliminate_outliers(self,labels,M):
         norms=np.zeros((len(labels)),np.float32)
         indices=np.zeros((len(labels)),np.float32)
-        #for i, cluster in enumerate(labels):
+ 
         for i, cluster in tqdm(enumerate(labels),total=len(labels)):
             if cluster == 0:
                 continue
@@ -145,8 +84,8 @@ class Clusterer(object):
         dfh['z1'] = dfh['z']/dfh['rt']        
         dz = 0.000015
         stepdz = 0.000001
-        steps = 70
-        #for ii in range(steps):
+        stepeps = 0.0000225
+        steps = 110
         for ii in tqdm(range(steps)):
             dz = dz + ii*stepdz
             dfh['a1'] = dfh['a0']+dz*dfh['z']*np.sign(dfh['z'].values)
@@ -156,7 +95,9 @@ class Clusterer(object):
             ss = StandardScaler()
             dfs = ss.fit_transform(dfh[['a1','z1','x1','x2','x3']].values)
       
-            self.clusters = DBSCAN(eps=0.0033-dz,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+            #self.clusters = DBSCAN(eps=0.0033-dz,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+            self.clusters = DBSCAN(eps=0.0033-ii*stepeps,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+
             if ii==0:
                 dfh['s1']=self.clusters
                 dfh['N1'] = dfh.groupby('s1')['s1'].transform('count')
@@ -173,7 +114,7 @@ class Clusterer(object):
                 dfh['N1'] = dfh.groupby('s1')['s1'].transform('count')
         dz = 0.000015
         stepdz = -0.000001
-        #for ii in range(steps):
+        stepeps = -0.0000225
         for ii in tqdm(range(steps)):
             dz = dz + ii*stepdz
             dfh['a1'] = dfh['a0']+dz*dfh['z']*np.sign(dfh['z'].values)
@@ -183,7 +124,10 @@ class Clusterer(object):
             ss = StandardScaler()
             dfs = ss.fit_transform(dfh[['a1','z1','x1','x2','x3']].values)
        
-            self.clusters = DBSCAN(eps=0.0033+dz,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+            #self.clusters = DBSCAN(eps=0.0033+dz,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+            self.clusters = DBSCAN(eps=0.0033+ii*stepeps,min_samples=1,metric='euclidean', n_jobs=8).fit(dfs).labels_
+
+
             dfh['s2'] = self.clusters
             dfh['N2'] = dfh.groupby('s2')['s2'].transform('count')
             maxs1 = dfh['s1'].max()
@@ -217,7 +161,7 @@ def create_one_event_submission(event_id, hits, labels):
     return submission
 
 def predict_create_submission_in_batch(events_id_list, hits_list, truth_list):
-    num_threads = len(event_id_list)
+    num_threads = len(events_id_list)
     print('num_threads: ' + str(num_threads))
     submissions = []
     scores = []
